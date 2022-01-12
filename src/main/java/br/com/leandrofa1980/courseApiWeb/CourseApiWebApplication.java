def5e95b7e7138fb3c9e0f2196f10dec3c6b8615
Leandro Ferreira <leandrofa1980@gmail.com>
2022-01-12 19:07:36 -0300

    Project created

---
+++ src/main/java/br/com/leandrofa1980/courseApiWeb/CourseApiWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandrofa1980.courseApiWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandrofa1980/courseApiWeb/CourseApiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrofa1980.courseApiWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
